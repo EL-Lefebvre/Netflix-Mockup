63d699c21356e00167baa2fa3ee791bc3f809e97
Ellie <elliemoon25@gmail.com>
2024-09-16 17:58:40 -0400

    added icons and styling

--- src/features/components/Footer/Footer.tsx
+++ src/features/components/Footer/Footer.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
-import "./Footer.css"; // Import the CSS file for styling
+import { IconButton } from "@mui/material";
+
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import XIcon from "@mui/icons-material/X";
+import "./Footer.css";
 
 const Footer = () => {
   const itemList = [
@@ -22,15 +28,53 @@ const Footer = () => {
 
   return (
     <Box className="footer">
-      <Box className="footer-content">
+      <Box className="iconsContainer">
+        <Box>
+          <IconButton
+            style={{
+              color: "white",
+            }}
+          >
+            <InstagramIcon />
+          </IconButton>
+        </Box>
+        <Box>
+          <IconButton
+            style={{
+              color: "white",
+            }}
+          >
+            <FacebookIcon />
+          </IconButton>
+        </Box>
+        <Box>
+          <IconButton
+            style={{
+              color: "white",
+            }}
+          >
+            <XIcon />
+          </IconButton>
+        </Box>
+        <Box>
+          <IconButton
+            style={{
+              color: "white",
+            }}
+          >
+            <YouTubeIcon />
+          </IconButton>
+        </Box>
+      </Box>
+      <Box className="footerContent">
         {itemList.map((item, index) => (
-          <Typography key={index} variant="body2" className="footer-link">
+          <Typography key={index} variant="body2" className="footerLink">
             {item}
           </Typography>
         ))}
       </Box>
-      <Box className="footer-padding">
-        <Typography variant="body1" className="footer-text">
+      <Box className="footerPadding">
+        <Typography variant="body2" className="footerText">
           © {new Date().getFullYear()} MovieFlix. All rights reserved.
         </Typography>
       </Box>
